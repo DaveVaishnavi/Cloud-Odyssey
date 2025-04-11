dffdb8f371ec85f49d45fd816cf79c3883e6038a
Kumud2908 <sagarkumud561@gmail.com>
2025-04-11 08:15:51 +0530

    feat(auth-service):add initial authentication microservice setup

---
+++ services/auth-service/database/databaseConnection.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	MongoDb := os.Getenv("MONGOURI")
+	fmt.Print(MongoDb)
+	clientOptions := options.Client().ApplyURI(MongoDb)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb")
+	return client
+
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("hospital").Collection(collectionName)
+	return collection
+}
